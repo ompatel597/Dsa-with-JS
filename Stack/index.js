fd44413921b21742b1f903a6e555c9d352aaaf34
ompatel597 <ompatel597@gmail.com>
2025-01-12 16:53:42 +0530

    stack done - 1

--- Stack/index.js
+++ Stack/index.js
@@ -1,21 +1,55 @@
-class Stack{
-    constructor(){
-        this.stack = []
-    }
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
 
-    push(data){
-        this.stack.push(data)
-    }
+  push(data) {
+    this.stack.push(data);
+  }
 
-    pop(){
-        this.stack.pop()
-    }
+  pop() {
+    this.stack.pop();
+  }
 
-    peek(){
-       return this.stack[this.stack.length - 1]
-    }
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
 
-    isEmpty(){
-        return this.stack.length === 0
+  isEmpty() {
+    return this.stack.length === 0;
+  }
+
+  size() {
+    return this.stack.length;
+  }
+
+  clear() {
+    this.stack = [];
+  }
+
+  contains(element) {
+    return this.stack.includes(element);
+  }
+
+  reverse() {
+    this.stack.reverse();
+  }
+
+  printStack() {
+    let st  r = "";
+
+    for (let i = 0; i < this.stack.length; i++) {
+      str += this.stack[i] + "\n";
+      return str;
     }
+  }
 }
+
+//usage Examples
+let myStack = new Stack();
+myStack.push(4);
+myStack.push(3);
+myStack.push(2);
+myStack.pop();
+
+console.log(myStack.printStack());
